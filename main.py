92c6353d22d3793793abaf1e3b83e6fff076e914
Yash7689 <69722604+Yash7689@users.noreply.github.com>
2021-07-16 14:27:45 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import fileinput as fi
+# This module replaces the word <|SPACE|> with a new line (code line 18)
+
+def writer():
+    with open("c:/PycharmProjects/copy_data_from_1_file_to_another/input.txt", "w") as writer:
+        data = input("Whatever you will write will be present in input.txt - ")
+        writer.write(data)
+# This is a input function whatever you will write that will come in input.txt
+
+def copy():
+    with open("c:/PycharmProjects/copy_data_from_1_file_to_another/input.txt", "r") as f:
+        with open("c:/PycharmProjects/copy_data_from_1_file_to_another/copyied.txt", "w") as f1:
+            font = f.read()
+            f1.write(font)
+# This is a function to copy data from input.txt and paste it in copyied.txt
+
+def editer():
+    with fi.FileInput("c:/PycharmProjects/copy_data_from_1_file_to_another/bajaj.txt", inplace=True, backup=".backup") as r:
+        for line in r:
+            print(line.replace(' ', '''
+'''), end='')
+# This function replaces <|SPACE|> with new line
+
+if __name__ == '__main__':
+    writer()
+    copy()
+    editer()
+# This will run the code
